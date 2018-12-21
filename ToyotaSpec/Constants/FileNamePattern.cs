8098ffbfd26633b9ba14273dd662d56b68b7132e
Siarhei.Klybik <Siarhei.Klybik@WSE-105-71>
2018-12-21 15:07:11 +0300

    Add excel utils. Parse excel to list of objects

--- ToyotaSpec/Constants/FileNamePattern.cs
+++ ToyotaSpec/Constants/FileNamePattern.cs
@@ -7,5 +7,6 @@ namespace ToyotaSpec.Constants
     static class FileNamePattern
     {
         public const string VinWalkCsv = "vinwalk_*.csv";
+        public const string VinWalkXlsx = "vinwalk_*.xlsx";
     }
 }
--- ToyotaSpec/ExportTest.cs
+++ ToyotaSpec/ExportTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using TestFramework.Utils;
 using ToyotaSpec.Constants;
@@ -21,7 +22,7 @@ namespace ToyotaSpec
         }
 
         [TestMethod]
-        public void VinWalkTest()
+        public void ExportCsv()
         {
             var softAssertions = new SoftAssertions();
             var loginPage = new LoginPage();
@@ -45,5 +46,29 @@ namespace ToyotaSpec
             softAssertions.AssertAll();
         }
 
+        [TestMethod]
+        public void ExportExcel()
+        {
+            var softAssertions = new SoftAssertions();
+            var loginPage = new LoginPage();
+            loginPage.LogIn(TestData.Login, TestData.Password);
+            var homePage = new HomePage();
+            homePage.TopMenuForm.NavigateTo(TopMenuItem.Reports);
+            var reportsPage = new ReportsPage();
+            reportsPage.NavigateTo(ReportsFormItem.VinWalk);
+            var vinWalkPage = new VinWalkPage();
+            var reportForm = new ReportForm(ReportName.VinWalk);
+            reportForm.ClickUpdate();
+            vinWalkPage.ClickExportExcel();
+            var downloadedFile = DownloadUtil.WaitForDownload(FileNamePattern.VinWalkXlsx).First();
+            var excel = new ExcelUtils(downloadedFile);
+            var excelTabular = excel.ExcelWorksheet().GetListOf<VinWalkTabular>(new Dictionary<string, string>{{"Misc.", "Misc"}});
+            var uiTabular = vinWalkPage.GetFullTabular();
+            for (var index = 0; index < uiTabular.Count; index++)
+            {
+                softAssertions.IsTrue(uiTabular[index].Equals(excelTabular[index]), $"Car from UI row={index + 1}, VIN={uiTabular[index].VIN} is not equal car from Excel row={index + 1}, VIN={excelTabular[index].VIN}");
+            }
+            softAssertions.AssertAll();
+        }
     }
 }
